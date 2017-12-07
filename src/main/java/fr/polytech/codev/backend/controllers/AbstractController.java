f087e32df00fd9c21d12a333b6a3ce3b2e4c64c3
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-12-07 16:57:04 +0100

    Squashed commit of the following:
    commit 96612ae18fdcc8d58b0fc6cecce7459acc573194
    Author: DELORME Loïc <loic.delorme.pro@gmail.com>
    Date:   Thu Dec 7 16:47:14 2017 +0100
    
        Add all operations on cryptocurrencies from admin
    
    commit cfad7ea9d3e6573b95a210c65ca8e682dcbae069
    Author: DELORME Loïc <loic.delorme.pro@gmail.com>
    Date:   Thu Dec 7 16:36:46 2017 +0100
    
        Add get one cryptocurrencies controller
    
    commit 66ed6323ccc56908ed1663e2a5e846377f50cc36
    Author: DELORME Loïc <loic.delorme.pro@gmail.com>
    Date:   Thu Dec 7 16:32:54 2017 +0100
    
        Add get all available cryptocurrencies controller
    
    commit 76a27360a761faebac11394f72b1832ad340bbf7
    Author: DELORME Loïc <loic.delorme.pro@gmail.com>
    Date:   Thu Dec 7 16:26:34 2017 +0100
    
        Add AbstractController class

---
+++ src/main/java/fr/polytech/codev/backend/controllers/AbstractController.java
@@ -0,0 +1,131 @@
+package fr.polytech.codev.backend.controllers;
+
+import fr.polytech.codev.backend.deserializers.AbstractStringDeserializer;
+import fr.polytech.codev.backend.deserializers.JsonStringDeserializer;
+import fr.polytech.codev.backend.entities.Entity;
+import fr.polytech.codev.backend.entities.Token;
+import fr.polytech.codev.backend.exceptions.ExpiredTokenException;
+import fr.polytech.codev.backend.exceptions.InvalidEntityException;
+import fr.polytech.codev.backend.exceptions.InvalidTokenException;
+import fr.polytech.codev.backend.exceptions.UnauthorizedUserException;
+import fr.polytech.codev.backend.serializers.AbstractStringSerializer;
+import fr.polytech.codev.backend.serializers.JsonStringSerializer;
+import fr.polytech.codev.backend.services.TokenSqlDaoServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public abstract class AbstractController {
+
+    private static Logger logger = LoggerFactory.getLogger(AbstractController.class);
+
+    private static AbstractStringSerializer abstractStringSerializer = new JsonStringSerializer();
+
+    private static AbstractStringDeserializer abstractStringDeserializer = new JsonStringDeserializer();
+
+    @Autowired
+    private TokenSqlDaoServices tokenSqlDaoServices;
+
+    public void logInfo(String message) {
+        logger.info(message);
+    }
+
+    public void logDebug(String message) {
+        logger.debug(message);
+    }
+
+    public void logWarn(String message) {
+        logger.warn(message);
+    }
+
+    public void logError(String message) {
+        logger.error(message);
+    }
+
+    public <I> String serialize(I data) {
+        return abstractStringSerializer.to(data);
+    }
+
+    public <O> O deserialize(String data, Class<O> outputType) {
+        return abstractStringDeserializer.from(data, outputType);
+    }
+
+    public void validate(Entity entity) throws InvalidEntityException {
+        final ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        final Validator validator = factory.getValidator();
+
+        final Set<ConstraintViolation<Entity>> violations = validator.validate(entity);
+        if (!violations.isEmpty()) {
+            throw new InvalidEntityException(violations.stream().map(violation -> violation.getMessage()).collect(Collectors.joining(", ")));
+        }
+    }
+
+    public void assertUserIsUser(String tokenValue, int requestedId) throws InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        final Token token = getToken(tokenValue);
+        assertTokenIsValid(token);
+        assertUserIsEnabled(token);
+        assertUserIsUser(token, requestedId);
+    }
+
+    public void assertUserIsAdministrator(String tokenValue) throws InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        final Token token = getToken(tokenValue);
+        assertTokenIsValid(token);
+        assertUserIsEnabled(token);
+        assertUserIsAdministrator(token);
+    }
+
+    public void assertUserIsUserOrAdministrator(String tokenValue, int id) throws InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        try {
+            assertUserIsUser(tokenValue, id);
+        } catch (InvalidTokenException | ExpiredTokenException | UnauthorizedUserException exception) {
+            assertUserIsAdministrator(tokenValue);
+        }
+    }
+
+    private Token getToken(String tokenValue) throws InvalidTokenException {
+        final Map<String, String> parameters = new HashMap<String, String>();
+        parameters.put("value", tokenValue);
+
+        final List<Token> tokens = this.tokenSqlDaoServices.filter(parameters);
+        if (tokens.size() != 1) {
+            throw new InvalidTokenException();
+        }
+
+        return tokens.get(0);
+    }
+
+    private void assertTokenIsValid(Token token) throws ExpiredTokenException {
+        if (token.getEndDate().isBefore(LocalDateTime.now())) {
+            throw new ExpiredTokenException();
+        }
+    }
+
+    private void assertUserIsEnabled(Token token) throws UnauthorizedUserException {
+        if (!token.getUser().isEnabled()) {
+            throw new UnauthorizedUserException();
+        }
+    }
+
+    private void assertUserIsUser(Token token, int requestedId) throws UnauthorizedUserException {
+        if (token.getUser().getId() != requestedId) {
+            throw new UnauthorizedUserException();
+        }
+    }
+
+    private void assertUserIsAdministrator(Token token) throws UnauthorizedUserException {
+        if (!token.getUser().isAdministrator()) {
+            throw new UnauthorizedUserException();
+        }
+    }
+}
