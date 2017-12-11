bc75ca6ed7f6ae1f7e65816781717f3f958310b6
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-12-11 09:38:08 +0100

    Declare controller services as beans

--- src/main/java/fr/polytech/codev/backend/BackendApplication.java
+++ src/main/java/fr/polytech/codev/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package fr.polytech.codev.backend;
 
+import fr.polytech.codev.backend.controllers.services.*;
 import fr.polytech.codev.backend.services.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -60,5 +61,55 @@ public class BackendApplication {
     @Bean
     public WalletSqlDaoServices walletSqlDaoServices() {
         return new WalletSqlDaoServices();
+    }
+
+    @Bean
+    public AlertControllerServices alertControllerServices() {
+        return new AlertControllerServices();
+    }
+
+    @Bean
+    public AlertTypeControllerServices alertTypeControllerServices() {
+        return new AlertTypeControllerServices();
+    }
+
+    @Bean
+    public AssetControllerServices assetControllerServices() {
+        return new AssetControllerServices();
+    }
+
+    @Bean
+    public CryptocurrencyControllerServices cryptocurrencyControllerServices() {
+        return new CryptocurrencyControllerServices();
+    }
+
+    @Bean
+    public FavoriteControllerServices favoriteControllerServices() {
+        return new FavoriteControllerServices();
+    }
+
+    @Bean
+    public LogControllerServices logControllerServices() {
+        return new LogControllerServices();
+    }
+
+    @Bean
+    public SettingControllerServices settingControllerServices() {
+        return new SettingControllerServices();
+    }
+
+    @Bean
+    public TokenControllerServices tokenControllerServices() {
+        return new TokenControllerServices();
+    }
+
+    @Bean
+    public UserControllerServices userControllerServices() {
+        return new UserControllerServices();
+    }
+
+    @Bean
+    public WalletControllerServices walletControllerServices() {
+        return new WalletControllerServices();
     }
 }
