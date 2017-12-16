df91e2a027fa1f3e7027b42c19ebc5da4cbf21f4
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-12-16 16:47:17 +0100

    Rename tokenValue into token

--- src/main/java/fr/polytech/codev/backend/controllers/administrators/AdministratorAssetController.java
+++ src/main/java/fr/polytech/codev/backend/controllers/administrators/AdministratorAssetController.java
@@ -1,9 +1,9 @@
 package fr.polytech.codev.backend.controllers.administrators;
 
 import fr.polytech.codev.backend.controllers.AbstractController;
-import fr.polytech.codev.backend.services.impl.AssetServices;
 import fr.polytech.codev.backend.exceptions.*;
 import fr.polytech.codev.backend.forms.AssetForm;
+import fr.polytech.codev.backend.services.impl.AssetServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,51 +11,51 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/cryptowallet/administrator/{tokenValue}/asset")
+@RequestMapping("/api/cryptowallet/administrator/{token}/asset")
 public class AdministratorAssetController extends AbstractController {
 
     @Autowired
     private AssetServices assetServices;
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity all(@PathVariable String tokenValue) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity all(@PathVariable String token) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.assetServices.all());
     }
 
     @RequestMapping(value = "wallet/{walletId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity get(@PathVariable String tokenValue, @PathVariable int walletId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity get(@PathVariable String token, @PathVariable int walletId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.assetServices.get(walletId, cryptocurrencyId));
     }
 
     @RequestMapping(value = "wallet/{walletId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity getByWallet(@PathVariable String tokenValue, @PathVariable int walletId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity getByWallet(@PathVariable String token, @PathVariable int walletId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.assetServices.getByWallet(walletId));
     }
 
     @RequestMapping(value = "cryptocurrency/{cryptocurrencyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity getByCryptocurrency(@PathVariable String tokenValue, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity getByCryptocurrency(@PathVariable String token, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.assetServices.getByCryptocurrency(cryptocurrencyId));
     }
 
     @RequestMapping(value = "wallet/{walletId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity insert(@PathVariable String tokenValue, @PathVariable int walletId, @PathVariable int cryptocurrencyId, @RequestBody String data) throws UnknownEntityException, InvalidEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity insert(@PathVariable String token, @PathVariable int walletId, @PathVariable int cryptocurrencyId, @RequestBody String data) throws UnknownEntityException, InvalidEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.assetServices.insert(walletId, cryptocurrencyId, deserialize(data, AssetForm.class)));
     }
 
     @RequestMapping(value = "wallet/{walletId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity update(@PathVariable String tokenValue, @PathVariable int walletId, @PathVariable int cryptocurrencyId, @RequestBody String data) throws UnknownEntityException, InvalidEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity update(@PathVariable String token, @PathVariable int walletId, @PathVariable int cryptocurrencyId, @RequestBody String data) throws UnknownEntityException, InvalidEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.assetServices.update(walletId, cryptocurrencyId, deserialize(data, AssetForm.class)));
     }
 
     @RequestMapping(value = "wallet/{walletId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity delete(@PathVariable String tokenValue, @PathVariable int walletId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity delete(@PathVariable String token, @PathVariable int walletId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         this.assetServices.delete(walletId, cryptocurrencyId);
         return serializeSuccessResponse();
     }
--- src/main/java/fr/polytech/codev/backend/controllers/administrators/AdministratorFavoriteController.java
+++ src/main/java/fr/polytech/codev/backend/controllers/administrators/AdministratorFavoriteController.java
@@ -1,9 +1,9 @@
 package fr.polytech.codev.backend.controllers.administrators;
 
 import fr.polytech.codev.backend.controllers.AbstractController;
-import fr.polytech.codev.backend.services.impl.FavoriteServices;
 import fr.polytech.codev.backend.exceptions.*;
 import fr.polytech.codev.backend.forms.FavoriteForm;
+import fr.polytech.codev.backend.services.impl.FavoriteServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,45 +11,45 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/api/cryptowallet/administrator/{tokenValue}/favorite")
+@RequestMapping("/api/cryptowallet/administrator/{token}/favorite")
 public class AdministratorFavoriteController extends AbstractController {
 
     @Autowired
     private FavoriteServices favoriteServices;
 
     @RequestMapping(value = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity all(@PathVariable String tokenValue) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity all(@PathVariable String token) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.favoriteServices.all());
     }
 
-    @RequestMapping(value = "user/{userId}/cryptocurrency{cryptocurrencyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity get(@PathVariable String tokenValue, @PathVariable int userId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    @RequestMapping(value = "user/{userId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity get(@PathVariable String token, @PathVariable int userId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.favoriteServices.get(userId, cryptocurrencyId));
     }
 
     @RequestMapping(value = "user/{userId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity getByUser(@PathVariable String tokenValue, @PathVariable int userId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity getByUser(@PathVariable String token, @PathVariable int userId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.favoriteServices.getByUser(userId));
     }
 
     @RequestMapping(value = "cryptocurrency/{cryptocurrencyId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity getByCryptocurrency(@PathVariable String tokenValue, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    public ResponseEntity getByCryptocurrency(@PathVariable String token, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         return serializeSuccessResponse(this.favoriteServices.getByCryptocurrency(cryptocurrencyId));
     }
 
-    @RequestMapping(value = "user/{userId}/cryptocurrency{cryptocurrencyId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity insert(@PathVariable String tokenValue, @PathVariable int userId, @PathVariable int cryptocurrencyId, @RequestBody String data) throws UnknownEntityException, InvalidEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
-        return serializeSuccessResponse(this.favoriteServices.insert(userId, cryptocurrencyId, deserialize(data, FavoriteForm.class)));
+    @RequestMapping(value = "user/{userId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity insert(@PathVariable String token, @PathVariable int userId, @PathVariable int cryptocurrencyId, @RequestBody String data) throws UnknownEntityException, InvalidEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
+        return serializeSuccessResponse(this.favoriteServices.insert(userId, cryptocurrencyId, new FavoriteForm()));
     }
 
-    @RequestMapping(value = "user/{userId}/cryptocurrency{cryptocurrencyId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-    public ResponseEntity delete(@PathVariable String tokenValue, @PathVariable int userId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
-        assertUserIsAdministrator(tokenValue);
+    @RequestMapping(value = "user/{userId}/cryptocurrency/{cryptocurrencyId}", method = RequestMethod.DELETE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity delete(@PathVariable String token, @PathVariable int userId, @PathVariable int cryptocurrencyId) throws UnknownEntityException, InvalidTokenException, ExpiredTokenException, UnauthorizedUserException {
+        assertUserIsAdministrator(token);
         this.favoriteServices.delete(userId, cryptocurrencyId);
         return serializeSuccessResponse();
     }
