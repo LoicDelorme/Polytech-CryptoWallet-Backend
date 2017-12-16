95d990d8055220f2d25c3124f026d05916be0f2f
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-12-16 14:53:25 +0100

    Return « ID » value instead of « name » value

--- src/main/java/fr/polytech/codev/backend/adapters/CryptocurrencyAdapter.java
+++ src/main/java/fr/polytech/codev/backend/adapters/CryptocurrencyAdapter.java
@@ -3,18 +3,18 @@ package fr.polytech.codev.backend.adapters;
 import fr.polytech.codev.backend.entities.Cryptocurrency;
 
 import javax.json.Json;
-import javax.json.JsonString;
+import javax.json.JsonNumber;
 import javax.json.bind.adapter.JsonbAdapter;
 
-public class CryptocurrencyAdapter implements JsonbAdapter<Cryptocurrency, JsonString> {
+public class CryptocurrencyAdapter implements JsonbAdapter<Cryptocurrency, JsonNumber> {
 
     @Override
-    public JsonString adaptToJson(Cryptocurrency original) throws Exception {
-        return Json.createValue(original.getName());
+    public JsonNumber adaptToJson(Cryptocurrency original) throws Exception {
+        return Json.createValue(original.getId());
     }
 
     @Override
-    public Cryptocurrency adaptFromJson(JsonString adapted) throws Exception {
+    public Cryptocurrency adaptFromJson(JsonNumber adapted) throws Exception {
         throw new UnsupportedOperationException();
     }
 }
