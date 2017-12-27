2254e91fd6fbef4cc288d7ac2e8720f699abe86e
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-12-27 10:27:33 +0100

    Fix an ID mistake

--- scripts/sql/CREATE_SAMPLE_DATA.sql
+++ scripts/sql/CREATE_SAMPLE_DATA.sql
@@ -19,7 +19,7 @@ INSERT INTO settings VALUES (3, 'Setting n° 1', 'light', now(), now(), 2);
 INSERT INTO alerts VALUES (1, 'ETH = 0.002467', '0.002467', false, false, now(), now(), 1, 2, 1);
 INSERT INTO alerts VALUES (2, 'BCH = 0.00467', '0.00467', false, true, now(), now(), 1, 3, 1);
 INSERT INTO alerts VALUES (3, 'XRP = 0.0120467', '0.0120467', true, false, now(), now(), 1, 4, 1);
-INSERT INTO alerts VALUES (3, 'DASH = 0.012467', '0.012467', true, true, now(), now(), 1, 5, 1);
+INSERT INTO alerts VALUES (4, 'DASH = 0.012467', '0.012467', true, true, now(), now(), 1, 5, 1);
 
 INSERT INTO wallets VALUES (1, 'Wallet n° 1', now(), now(), 1);
 INSERT INTO wallets VALUES (2, 'Wallet n° 2', now(), now(), 1);
