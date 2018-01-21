939c12eb81455c2cc3a4c32ff6386d86e6a8a89b
DELORME Loïc <loic.delorme.pro@gmail.com>
2018-01-21 11:29:34 +0100

    Improve deployment scripts

---
+++ deployment.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+echo "##############################################################################"
+echo "#                                   DOCKER                                   #"
+echo "##############################################################################"
+
+echo "Updating the apt package index"
+apt-get update
+echo ""
+
+echo "Removing old versions of docker if exists"
+apt-get remove docker docker-engine docker.io
+echo ""
+
+echo "Removing current version of docker if exists"
+apt-get --assume-yes purge docker-ce
+echo ""
+
+echo "Remove old containers, images and networks if exists"
+rm -Rf /var/lib/docker
+echo ""
+
+echo "Updating the apt package index"
+apt-get update
+echo ""
+
+echo "Installing some packages for Docker"
+apt-get --assume-yes install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+echo ""
+
+echo "Adding Docker’s official GPG key"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+echo ""
+
+echo "Checking fingerprint"
+echo "YOU SHOULD HAVE 9DC8 5822 9FC7 DD38 854A E2D8 8D81 803C 0EBF CD88 fingerprint"
+apt-key fingerprint 0EBFCD88
+echo ""
+
+echo "Adding stable repository"
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+echo ""
+
+echo "Updating the apt package index"
+apt-get update
+echo ""
+
+echo "Installing Docker"
+apt-get --assume-yes install docker-ce
+echo ""
+
+echo "Displaying installed versions of Docker"
+apt-cache madison docker-ce
+echo ""
+
+echo "##############################################################################"
+echo "#                               DOCKER COMPOSE                               #"
+echo "##############################################################################"
+
+echo "Removing old versions of docker-compose if exists"
+rm -f /usr/local/bin/docker-compose
+echo ""
+
+echo "Downloading last version of docker-compose"
+curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+echo ""
+
+echo "Making docker-compose executable"
+chmod +x /usr/local/bin/docker-compose
+echo ""
+
+echo "Checking docker-compose version"
+docker-compose --version
+echo ""
+
+echo "##############################################################################"
+echo "#                             SCRIPTS DEPLOYMENT                             #"
+echo "##############################################################################"
+
+echo "Downloading deployment script"
+curl -L https://raw.githubusercontent.com/LoicDelorme/Polytech-CryptoWallet-Backend/master/docker-compose.yml?token=AMTU8Qp_Zafv9L-5bSawqjgyZxcvuohzks5abZt7wA%3D%3D -o docker-compose.yml
+echo ""
+
+echo "Deploying all containers"
+docker-compose up -d mysql-db-container
+sleep 20
+docker-compose up -d cryptowallet-backend-container
+echo ""
+
+echo "##############################################################################"
+echo "#                                 RUNNING?                                   #"
+echo "##############################################################################"
+
+echo "Listing all downloaded images"
+docker images
+echo ""
+
+echo "Listing all running containers"
+docker ps -a
+echo ""
+
+echo "##############################################################################"
+echo "#                                  NETWORK                                   #"
+echo "##############################################################################"
+
+echo "Listing all network interfaces"
+netstat -ntlp | grep LISTEN
+echo ""
