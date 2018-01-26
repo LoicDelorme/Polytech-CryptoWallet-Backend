1d6232133270f7d58fe6801449eaf0bbbfd5ffa3
DELORME Loïc <loic.delorme.pro@gmail.com>
2018-01-26 08:46:30 +0100

    Enable SpringBoot scheduling

--- src/main/java/fr/polytech/codev/backend/BackendApplication.java
+++ src/main/java/fr/polytech/codev/backend/BackendApplication.java
@@ -7,8 +7,10 @@ import fr.polytech.codev.backend.services.impl.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BackendApplication {
 
     public static void main(String[] args) {
