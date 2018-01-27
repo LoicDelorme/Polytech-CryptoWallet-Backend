0fad85627761eddead3fa8b04f22c71ee5f189e6
DELORME Loïc <loic.delorme.pro@gmail.com>
2018-01-27 15:15:22 +0100

    Add CoinMarketCap controller

---
+++ src/main/java/fr/polytech/codev/backend/controllers/coinmarketcap/CoinMarketCapController.java
@@ -0,0 +1,32 @@
+package fr.polytech.codev.backend.controllers.coinmarketcap;
+
+import fr.polytech.codev.backend.controllers.AbstractController;
+import fr.polytech.codev.backend.services.impl.CoinMarketCapServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/cryptowallet/coinmarketcap")
+public class CoinMarketCapController extends AbstractController {
+
+    @Autowired
+    private CoinMarketCapServices coinMarketCapServices;
+
+    @RequestMapping(value = "/graphs/{cryptocurrencyResourceUrl}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity allPrices(@PathVariable String cryptocurrencyResourceUrl) {
+        return serializeSuccessResponse(this.coinMarketCapServices.allPrices(cryptocurrencyResourceUrl));
+    }
+
+    @RequestMapping(value = "/graphs/{cryptocurrencyResourceUrl}/{startDate}/{endDate}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity allPricesBetween(@PathVariable String cryptocurrencyResourceUrl, @PathVariable String startDate, @PathVariable String endDate) {
+        return serializeSuccessResponse(this.coinMarketCapServices.allPricesBetween(cryptocurrencyResourceUrl, startDate, endDate));
+    }
+
+    @RequestMapping(value = "/ticker/{cryptocurrencyResourceUrl}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity getCurrentPrice(@PathVariable String cryptocurrencyResourceUrl) {
+        return serializeSuccessResponse(this.coinMarketCapServices.getCurrentPrice(cryptocurrencyResourceUrl));
+    }
+}
